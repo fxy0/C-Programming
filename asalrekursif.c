c48edee32c768ce48605a06cb7ce97e9123cfc0d
fxy0 <32413273+fxy0@users.noreply.github.com>
2017-11-14 00:16:05 +0300

    Update asalrekursif.c

--- asalrekursif.c
+++ asalrekursif.c
@@ -6,24 +6,7 @@
 
 int asalrekursif(int asal,int i)
 {
-  /*  int i,j,s;
-    for(i=2;i<10;i++){
-
-s=0;
-        for(j=2;j<i;j++)
-        {
-             printf("Tur = %d\n",j);
-             printf("--%d--%d--\n",i,j);
-             if(i%j==0)
-                s=1;
-        }
-        if(s==0)
-        printf("Asaldir=%d\n",i);
-    }
-
-return 1;
-*/
-
+  
 if(i==1)
     return 1;
 else
